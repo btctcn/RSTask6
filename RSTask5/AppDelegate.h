88fd5134c0e0741da1083a4688eaaa285856e946
Andrey Butcitcyn <btctcn@gmail.com>
2020-06-19 23:47:40 +0300

    initial commit

---
+++ RSTask5/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RSTask5
+//
+//  Created by Andrey Butcitcyn on 19.06.2020.
+//  Copyright © 2020 Andrey Butcitcyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (nullable, nonatomic, strong) UIWindow *window;
+
+@end
+
