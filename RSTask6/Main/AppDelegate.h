4af2ad2915c1006497a6ecf8e20f04ae8e37382d
Andrey Butcitcyn <btctcn@gmail.com>
2020-06-22 13:37:41 +0300

    build fix

--- RSTask6/Main/AppDelegate.h
+++ RSTask6/Main/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  RSTask5
-//
-//  Created by Andrey Butcitcyn on 19.06.2020.
-//  Copyright © 2020 Andrey Butcitcyn. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
